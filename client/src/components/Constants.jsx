3b27cb32e073bae0ef205c9b1905c8c7fd14227f
ClintTheEngineer <reachme@clintalexander.dev>
2024-09-28 23:43:11 -0400

    minor fixes

--- client/src/components/Constants.jsx
+++ client/src/components/Constants.jsx
@@ -1,7 +1,7 @@
 export const Constants = {
     SERVER_URL: (() => {
         const currentBaseUrl = window.location.hostname;
-        if (currentBaseUrl === 'cander-portfolio-cms.netlify.app') {
+        if (currentBaseUrl === 'cander-portfolio-cms.netlify.app' || currentBaseUrl ==='https://66f8c7b71465edade6106d19--cander-portfolio-cms.netlify.app/login') {
             return 'https://portfolio-cms-server-eiwk.onrender.com';
         } else {
             return 'http://localhost:5000';

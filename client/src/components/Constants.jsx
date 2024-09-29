6e117b1adb981557068d00e8ab450078c36bf372
ClintTheEngineer <reachme@clintalexander.dev>
2024-09-28 23:50:16 -0400

    clean

--- client/src/components/Constants.jsx
+++ client/src/components/Constants.jsx
@@ -1,10 +1,10 @@
 export const Constants = {
     SERVER_URL: (() => {
         const currentBaseUrl = window.location.hostname;
-        if (currentBaseUrl === 'cander-portfolio-cms.netlify.app' || currentBaseUrl ==='https://66f8c7b71465edade6106d19--cander-portfolio-cms.netlify.app/login') {
+        if (currentBaseUrl === 'cander-portfolio-cms.netlify.app') {
             return 'https://portfolio-cms-server-eiwk.onrender.com';
         } else {
-            return 'http://localhost:5000';
+            return 'https://portfolio-cms-server-eiwk.onrender.com';
         }
     })(),
     APP_NAME: 'Cander CMS 🗝️'
